0b79637a6253d17f19a8ee382e473da757376fc7
lxingze <lxzup9@163.com>
2021-12-03 17:24:06 +0800

    Site updated: 2021-12-03 17:24:06

---
+++ js/daovoice.js
@@ -0,0 +1,15 @@
+(function (i, s, o, g, r, a, m) {
+    i["DaoVoiceObject"] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    a.charset = "utf-8";
+    m.parentNode.insertBefore(a, m)
+})(window, document, "script", ('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/f84bbafb.js", "daovoice");
+daovoice('init', {
+    app_id: "f84bbafb"
+});
+daovoice('update');
